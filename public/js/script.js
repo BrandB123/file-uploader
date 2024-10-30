fa4d5ee64968236f9d62d85ef40c2e0899dd8a08
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2024-10-30 02:19:53 -0500

    Link css and js files

---
+++ public/js/script.js
@@ -0,0 +1,23 @@
+// console.log("Hello world");
+
+function dontAddUser(event){
+    passwordConfirmationInput.style = "border-color: red;"
+    event.preventDefault()
+}
+
+const passwordInput = document.querySelector("#password");
+const passwordConfirmationInput = document.querySelector("#password-confirmation");
+const submitButton = document.querySelector("#add-user-button")
+
+passwordConfirmationInput.addEventListener("change", () => {
+    if (passwordInput.value !== passwordConfirmationInput.value){
+        submitButton.addEventListener("click", dontAddUser)
+    }
+})
+
+passwordConfirmationInput.addEventListener("change", () => {
+    if (passwordInput.value === passwordConfirmationInput.value){
+        passwordConfirmationInput.style = "border-color: black"
+        submitButton.removeEventListener("click", dontAddUser)
+    }
+})
