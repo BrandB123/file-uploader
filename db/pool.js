4277eae44ffb8c19bd185ff306192e8b6f71a8fb
BrandB123 <153580357+BrandB123@users.noreply.github.com>
2024-11-05 08:20:04 -0600

    Update environment variable name

--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,5 @@ require('dotenv').config()
 const {Pool} = require('pg');
 
 module.exports = new Pool({
-    connectionString: process.env.CONNECTION_STRING
+    connectionString: process.env.DATABASE_PUBLIC_URL
 })
